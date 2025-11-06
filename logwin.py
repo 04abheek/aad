ce25177063b942bc79ea50d319d80aaa5a7d451b
04abheek <126752252+04abheek@users.noreply.github.com>
2025-11-07 00:14:44 +0530

    Add files via upload

---
+++ logwin.py
@@ -0,0 +1,10 @@
+1 Write a program to implement insertion sort and find the running time of the algorithm.
+2 Write a program to implement merge sort algorithm. Compare the time and memory complexity.
+3 Write a program on Strassen’s algorithm for matrix multiplication and analyse its complexity.
+4 Implement hiring problem and analyse its complexity.
+5 Write a program to implement Longest Common Subsequence (LCS) algorithm.
+6 Write a program to implement Huffman's code algorithm.
+7 Write a program to implement Kruskal's algorithm.
+8 Write a program to implement Dijkstra's algorithm.
+9 Write a program to implement multi-threaded computation concepts in the generation of Fibonacci numbers. (Generate first n Fib no’s)
+10 Implement Chinese reminder theorem to a constraint satisfaction problem.
